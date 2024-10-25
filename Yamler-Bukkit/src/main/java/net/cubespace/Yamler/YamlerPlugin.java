b47ec6e0a36ab00294a4f44e56e93cf4bef939be
Kronnox <root@gituser.de>
2024-10-25 11:40:11 +0200

    add folia support & update deploy details

--- Yamler-Bukkit/src/main/java/net/cubespace/Yamler/YamlerPlugin.java
+++ Yamler-Bukkit/src/main/java/net/cubespace/Yamler/YamlerPlugin.java
@@ -2,19 +2,18 @@ package net.cubespace.Yamler;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
  */
+@SuppressWarnings("unused")
 public class YamlerPlugin extends JavaPlugin {
     public void onEnable() {
-        try {
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-            getLogger().info("Could not start metrics");
-        }
+//        try {
+//            Metrics metrics = new Metrics(this);
+//            metrics.start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            getLogger().info("Could not start metrics");
+//        }
     }
 }
